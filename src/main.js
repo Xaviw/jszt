f1ab69d5e0b7f0b1b4eb1da5ada0d45f8ab5589f
xavi <xaviw@foxmail.com>
2023-04-30 18:08:46 +0800

    initial

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = []
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
